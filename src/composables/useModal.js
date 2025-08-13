1aa80f476708bfe0e237943f54b627954fcf7497
Krish A. Patel <krishpatel2067@gmail.com>
2025-08-13 13:15:23 -0400

    Create useModal composable

---
+++ src/composables/useModal.js
@@ -0,0 +1,28 @@
+import { ref, readonly, shallowRef } from "vue";
+
+const modalStack = ref([]);
+
+export function useModal() {
+    const openModal = (options) => {
+        const id = Date.now() + Math.random();
+
+        modalStack.value.push({
+            id,
+            title: options.title,
+            component: shallowRef(options.component),
+            props: options.props || {},
+        });
+    };
+
+    const closeModal = () => {
+        if (modalStack.value.length > 0) {
+            modalStack.value.pop();
+        }
+    };
+
+    return {
+        modalStack: readonly(modalStack),
+        openModal,
+        closeModal,
+    };
+}

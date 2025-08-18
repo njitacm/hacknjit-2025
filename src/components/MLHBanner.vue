17f8247441877faaac0a9b7df2cad272c9a1770c
Krish A. Patel <krishpatel2067@gmail.com>
2025-08-17 20:45:54 -0400

    Clean up code

--- src/components/MLHBanner.vue
+++ src/components/MLHBanner.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- from the MLH website -->
   <a id="mlh-trust-badge"
     style="display:block;max-width:100px;min-width:60px;position:fixed;right:50px;top:0;width:10%;z-index:10000"
     href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white"
@@ -7,21 +8,3 @@
       alt="Major League Hacking 2026 Hackathon Season" style="width:100%">
   </a>
 </template>
-<style scoped>
-.banner {
-  display: block;
-  max-width: 100px;
-  min-width: 60px;
-  position: fixed;
-  right: 50px;
-  top: 0;
-  width: 10%;
-  z-index: 10000;
-}
-
-@media (max-width: 700px) {
-  .banner {
-    right: 5px;
-  }
-}
-</style>

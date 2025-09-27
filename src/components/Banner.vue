2c786699f871f25b51b388ea05fb0946abeeae02
Krish A. Patel <krishpatel2067@gmail.com>
2025-09-27 15:13:26 -0400

    Merge successful

---
+++ src/components/Banner.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="Banner">
+    <NotifSignup id="NotifSignup" />
+    <TheCountdown />
+    <div class="view-hint">
+      <span>View last year's photos</span>
+      <br />
+      <span>&#129123;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import NotifSignup from "./NotifSignup.vue";
+import TheCountdown from "./TheCountdown.vue";
+
+export default {
+  components: {
+    NotifSignup,
+    TheCountdown
+  }
+}
+</script>
+
+<style scoped>
+.Banner {
+  min-height: 100svh;
+  height: fit-content;
+  display: grid;
+  gap: 1rem;
+}
+
+.view-hint {
+  align-content: center;
+}
+
+.view-hint * {
+  margin: 0 auto;
+}
+</style>

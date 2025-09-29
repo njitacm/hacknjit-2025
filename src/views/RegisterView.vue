fb4ab7fdd297afed7fe7fe0825602477f0cd9ab7
Possiblyai <imrana50280@gmail.com>
2025-09-29 14:16:48 -0400

    Moved styling to specific views and fixed missing NavBar

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="body-container">
+    <Registration></Registration>
+  </main>
+</template>
+
+<script>
+import Registration from "../components/Registration.vue";
+
+export default {
+  name: "HackNJIT",
+  components: {
+    Registration,
+  }
+};
+</script>

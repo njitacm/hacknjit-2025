916900e4230d3d86f39e610510c5a59932234a0b
Possiblyai <imrana50280@gmail.com>
2025-09-29 14:20:56 -0400

    Fixed global styling

--- src/App.vue
+++ src/App.vue
@@ -113,6 +113,61 @@ body.modal-open {
   line-height: 1.5em;
 }
 
+p,
+span,
+a {
+  font-size: 1.25em;
+}
+
+h1,
+h2,
+h3 {
+  font-weight: bold;
+}
+
+h1 {
+  font-size: 6em;
+}
+
+h2 {
+  font-size: 3em;
+}
+
+h3 {
+  font-size: 1.5em;
+}
+
+@media (max-width: 1000px) {
+  h1 {
+    font-size: 4em;
+  }
+
+  h2 {
+    font-size: 2em;
+  }
+
+  h3 {
+    font-size: 1.25em;
+  }
+
+  p,
+  span,
+  a,
+  button {
+    font-size: 1em;
+  }
+}
+
+button {
+  font-size: 1.5em;
+  -webkit-tap-highlight-color: transparent;
+}
+
+.page-side-padding {
+  margin: 0 auto;
+  width: calc(100vw - 64px);
+}
+
 main {
   display: grid;
   gap: 32px;

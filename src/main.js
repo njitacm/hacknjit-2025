fa63d98ff846749cc05751faa540950c94acebd7
Possiblyai <imrana50280@gmail.com>
2025-09-29 13:59:36 -0400

    Swithed to using routing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router.js'
 
 var app = createApp(App);
 app.config.globalProperties.window = window
-
+app.use(router)
 app.mount('#app')

021a700224cd0706643a86186e2ef0d4b4444d3e
Krish A. Patel <krishpatel2067@gmail.com>
2025-09-30 14:07:16 -0400

    Fix routing and make it more robust

--- src/router.js
+++ src/router.js
@@ -4,11 +4,26 @@ import HomeView from './views/HomeView.vue'
 import RegisterView from './views/RegisterView.vue'
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/registration', component: RegisterView },
+  {
+    path: '/',
+    component: HomeView
+  },
+  {
+    path: '/registration',
+    component: RegisterView
+  },
 ]
 
 export default createRouter({
   history: createMemoryHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return {
+        top: 0,
+      }
+    }
+  }
 })

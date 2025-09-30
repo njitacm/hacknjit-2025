a65f068341654214beb09477c839293d9eeb083d
Krish A. Patel <krishpatel2067@gmail.com>
2025-09-29 22:54:13 -0400

    Use pinia

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router.js'
 
-var app = createApp(App);
+const pinia = createPinia()
+const app = createApp(App)
+app.use(pinia)
+
 app.config.globalProperties.window = window
 
 app.use(router)

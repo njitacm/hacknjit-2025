4be8aa2d312c8cb27caa5d2ed46ec9007a281339
Possiblyai <imrana50280@gmail.com>
2025-10-03 20:44:47 -0400

    Removed register view

--- src/router.js
+++ src/router.js
@@ -1,16 +1,11 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 import HomeView from './views/HomeView.vue'
-// import RouterView from './views/RouterView.vue'
 
 const routes = [
   {
     path: '/',
     component: HomeView
-  },
-  {
-    path: '/registration',
-    component: RegisterView
   },
 ]
 

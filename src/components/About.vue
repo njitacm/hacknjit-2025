dcc50483eb18004ff4ab278c2202a3878782507e
Krish A. Patel <krishpatel2067@gmail.com>
2025-10-08 15:11:21 -0400

    Create About component

---
+++ src/components/About.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="About" ref="sectionRef" id="About">
+    <h2>About</h2>
+    <div class="container">
+      <img />
+      <div class="text-content">
+        <p>HackNJIT is a 24-hour hackathon hosted anually at the New Jersey Institute of Technology. Our mission is to
+          empower students to create...</p>
+        <p><img />Where? </p>
+        <p><img />When? </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { observe, unobserve } from "../composables/useIntersectionObserver";
+
+export default {
+  mounted() {
+    observe(this.$refs.sectionRef);
+  },
+  unmounted() {
+    unobserve(this.$refs.sectionRef);
+  }
+}
+
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  gap: 16px
+}
+</style>

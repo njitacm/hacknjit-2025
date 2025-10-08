ed577e13ebccaa449d19908303e3df5fc59c5883
Krish A. Patel <krishpatel2067@gmail.com>
2025-10-08 15:15:02 -0400

    Add About component; reorder components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <!-- <Banner gradient="radial-gradient(yellow, green)" /> -->
     <Banner />
     <!-- <Tracks/> -->
+    <About />
     <TheSponsors />
     <TheFAQ />
     <PastPics />
@@ -10,24 +11,26 @@
 </template>
 
 <script>
-import TheFAQ from "../components/TheFAQ.vue";
-import TheSponsors from "../components/Sponsors.vue";
-import Modal from "../components/Modal.vue";
-import Banner from "../components/Banner.vue";
-import Tracks from "../components/Tracks.vue";
-import PastPics from "../components/PastPics.vue";
 import NavBar from "../components/NavBar.vue";
+import Banner from "../components/Banner.vue";
+import About from "../components/About.vue";
+import TheSponsors from "../components/Sponsors.vue";
+import TheFAQ from "../components/TheFAQ.vue";
+import PastPics from "../components/PastPics.vue";
+import Tracks from "../components/Tracks.vue";
+import Modal from "../components/Modal.vue";
 
 export default {
   name: "HackNJIT",
   components: {
-    TheFAQ,
-    TheSponsors,
-    Modal,
-    Banner,
-    Tracks,
-    PastPics,
     NavBar,
+    Banner,
+    About,
+    TheSponsors,
+    TheFAQ,
+    PastPics,
+    Tracks,
+    Modal,
   }
 };
 </script>

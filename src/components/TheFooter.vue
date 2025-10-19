b92367f2ce36e08e6b9f1ac092fae371666f074b
Krish A. Patel <krishpatel2067@gmail.com>
2025-10-19 19:37:52 -0400

    Redesign footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,7 +1,6 @@
 <template>
-  <footer>
-    <nav>
-      <hr />
+  <footer class="Footer section">
+    <div class="socials-container">
       <!-- <a href="https://discord.com" target="_blank"
         ><img src="../assets/logos/discord.svg"
       /></a>
@@ -13,27 +12,20 @@
       <a href="https://x.com/NJITACM" target="_blank"><img src="../assets/logos/twitter.svg" /></a>
       <a href="https://https://www.youtube.com/channel/UC3stSn7UK5-IX6mNnlWlRLw" target="_blank"><img
           src="../assets/logos/youtube.svg" /></a>
-      <hr />
-    </nav>
+    </div>
     <div class="NJIT_logos">
       <img id="acm-logo" src="../assets/logos/acm_logo_white.svg" />
       <img id="ywcc-logo" src="../assets/logos/NJIT_white.png" />
     </div>
-    <aside>
-      <hr />
+    <div class="links-container">
       <a href="https://acm.njit.edu" target="_blank">Main ACM Site</a>
-      <hr />
       <a href="https://www.acm.org/" target="_blank">ACM National</a>
-      <hr />
       <a href="https://computing.njit.edu/" target="_blank">NJIT Ying Wu College of Computing</a>
       <!-- <hr />
       <a href="https://acm.njit.edu" target="_blank">Sponsor Packet</a> -->
-      <hr />
       <a href="mailto:hacknjit@njit.edu" target="_blank">Email Us</a>
-      <hr />
       <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target="_blank">MLH Code of Conduct</a>
-      <hr />
-    </aside>
+    </div>
   </footer>
 </template>
 
@@ -42,48 +34,41 @@ export default {};
 </script>
 
 <style scoped>
-footer {
+.Footer {
+  margin-inline: auto;
+  margin-bottom: 32px;
+  border-radius: var(--border-radius);
+  background-color: var(--hacknjit-secondary);
   color: var(--text-color);
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  padding: 2rem;
-  padding-bottom: 2rem;
-}
-
-nav,
-aside {
-  display: flex;
-  flex-direction: row;
+  display: grid;
   justify-content: center;
-  gap: 1rem;
-  padding: 0.75rem 0;
+  gap: 16px;
+  padding: 32px;
 }
 
-aside {
-  flex-wrap: wrap;
-}
-
-div.line {
+.socials-container {
   display: flex;
-  align-content: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 32px;
 }
 
-nav hr {
-  border: none;
-  border-top: 4px solid white;
-  border-radius: 1000rem;
-  /* border-top: 3px solid black;
-  border-bottom: 3px solid black; */
-  width: 25%;
-  height: 1px;
-  align-self: center;
+.links-container {
+  --gap: 8px;
+  --cols: 2;
+  display: grid;
+  grid-template-columns: repeat(var(--cols), calc(100% / var(--cols) - var(--gap) / (var(--cols) - 1)));
+  gap: var(--gap);
+  align-content: center;
+  align-items: center;
+  width: 100%;
 }
 
 .NJIT_logos {
   display: flex;
   align-content: center;
   justify-content: center;
+  flex-wrap: wrap;
   gap: 0.5rem;
 }
 
@@ -96,28 +81,56 @@ nav hr {
   width: 200px;
 }
 
-nav img {
+.socials-container img {
   width: 50px;
   height: 50px;
 }
 
-a {
-  color: inherit;
+.links-container a {
+  --hov-act-col: var(--hacknjit-primary);
+  width: 100%;
+  border-radius: var(--border-radius);
+  color: white;
+  font-weight: bold;
+  text-decoration: none;
+  flex-grow: 1;
+  text-align: center;
+  padding: 8px 16px;
+}
+
+@media (hover: hover) and (pointer: fine) {
+  .links-container a:hover {
+    background-color: var(--hov-act-col);
+  }
+}
+
+@media (pointer: coarse) {
+  .links-container a:active {
+    background-color: var(--hov-act-col);
+  }
+}
+
+@media(max-width: 1000px) {
+  .Footer {
+    width: 100%;
+    border-radius: 0;
+  }
 }
 
 @media (max-width: 600px) {
-  nav img {
+  .socials-container img {
     width: 40px;
     height: 40px;
   }
 
-  aside {
-    font-size: 0.75rem;
+  .links-container {
+    --cols: 1;
+    grid-template-columns: repeat(var(--cols), calc(100% / var(--cols)));
   }
 }
 
 @media (max-width: 500px) {
-  nav img {
+  .socials-container img {
     width: 35px;
   }
 }

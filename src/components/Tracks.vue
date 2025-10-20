b504ad6522d3d4671fbd5c88c6f836d6378b550a
Krish A. Patel <krishpatel2067@gmail.com>
2025-10-20 18:59:47 -0400

    Background overlay

--- src/components/Tracks.vue
+++ src/components/Tracks.vue
@@ -37,9 +37,22 @@ export default {
 }
 
 .track-btn {
+  position: relative;
   border-radius: var(--border-radius);
   width: 100%;
   cursor: pointer;
   border: none;
+}
+
+/* semi-transparent overlay */
+.track-btn::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
 }
 </style>

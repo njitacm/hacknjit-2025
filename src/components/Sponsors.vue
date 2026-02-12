90d6b2a62cd54c1d37cdaca65f2508b608b7241e
Possiblyai <imrana50280@gmail.com>
2026-02-12 15:57:07 -0500

    Fixed sponsor images not appearing

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -35,7 +35,7 @@ onMounted(async () => {
   // companies in each tier
   for (const companies of Object.values(newSponsors)) {
     for (const company of companies) {
-      company.imgSrc = await getImageUrl(`sponsors/${company.imgSrc}`);
+      company.imgSrc = new URL(`../assets/sponsors/${company.imgSrc}`, import.meta.url).href;
     }
   }
 
